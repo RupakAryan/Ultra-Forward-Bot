0d53ed353ed3d2ab39061295ab42631696bfb1d8
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-08-21 19:07:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://rupakaryanaryan:rupakaryanaryan@cluster0.05xkjx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "rupakaryanaryan")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8404009236').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
